e65beb3e2d13d1355673fcf8e4786d55746b4fcf
Maurice Fahn <maurice.fahn@gmail.com>
2018-06-12 11:44:59 +0200

    DEV: Initial commit.

---
+++ CoordinatorPattern/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by Maurice Fahn on 12.06.18.
+//  Copyright © 2018 Maurice Fahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
